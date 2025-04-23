a69c7016a093e6dc708a6a5adc4f1c70938a696b
panindhra <phanindhracharigudipati@gmail.com>
2025-04-23 10:45:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int id = 234;
+        System.out.println("id:" +id);
         System.out.println("Hello, World!");
     }
 }
